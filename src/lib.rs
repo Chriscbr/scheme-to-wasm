32c74b3e027fda39c08d2bfd3d50b392119e634f
Chriscbr <crybicki98@gmail.com>
2019-09-09 20:59:07 -0400

    Add binop type checking

--- src/lib.rs
+++ src/lib.rs
@@ -21,14 +21,38 @@ impl std::error::Error for TypeCheckError {
     }
 }
 
-pub fn type_check(value: lexpr::Value) -> Result<Type, TypeCheckError> {
+pub fn type_check(value: &lexpr::Value) -> Result<Type, TypeCheckError> {
     match value {
         lexpr::Value::Number(_) => Ok(Type::Int),
         lexpr::Value::Bool(_) => Ok(Type::Bool),
         lexpr::Value::String(_) => Ok(Type::Str),
+        lexpr::Value::Cons(_) => {
+            if !value.is_list() {
+                return Err(TypeCheckError);
+            }
+            let lst = value.to_vec().unwrap();
+            let lst_parts = lst.split_at(1);
+            let first = lst_parts.0;
+            let rest = lst_parts.1;
+            if !(first[0]).is_symbol() {
+                return Err(TypeCheckError);
+            }
+            let operator = first[0].as_symbol().unwrap();
+            match operator {
+                "+" | "*" | "-" | "/" => {
+                    let e1 = type_check(&rest[0]).unwrap();
+                    let e2 = type_check(&rest[1]).unwrap();
+                    if e1 == Type::Int && e2 == Type::Int {
+                        return Ok(Type::Int);
+                    } else {
+                        return Err(TypeCheckError);
+                    }
+                },
+                _ => Err(TypeCheckError)
+            }
+        },
         lexpr::Value::Symbol(x) => {
-            let symbol = x.into_string();
-            match &symbol[..] {
+            match &x[..] {
                 "true" => Ok(Type::Bool),
                 "false" => Ok(Type::Bool),
                 _ => Err(TypeCheckError)
@@ -45,27 +69,45 @@ mod tests {
     #[test]
     fn typecheck_prims() {
         let exp = lexpr::from_str("3").unwrap();
-        assert_eq!(type_check(exp).unwrap(), Type::Int);
+        assert_eq!(type_check(&exp).unwrap(), Type::Int);
+
+        let exp = lexpr::from_str("-4").unwrap();
+        assert_eq!(type_check(&exp).unwrap(), Type::Int);
 
         let exp = lexpr::from_str("#t").unwrap();
-        assert_eq!(type_check(exp).unwrap(), Type::Bool);
+        assert_eq!(type_check(&exp).unwrap(), Type::Bool);
 
         let exp = lexpr::from_str("#f").unwrap();
-        assert_eq!(type_check(exp).unwrap(), Type::Bool);
+        assert_eq!(type_check(&exp).unwrap(), Type::Bool);
 
         let exp = lexpr::from_str("true").unwrap();
-        assert_eq!(type_check(exp).unwrap(), Type::Bool);
+        assert_eq!(type_check(&exp).unwrap(), Type::Bool);
 
         let exp = lexpr::from_str("false").unwrap();
-        assert_eq!(type_check(exp).unwrap(), Type::Bool);
+        assert_eq!(type_check(&exp).unwrap(), Type::Bool);
 
         let exp = lexpr::from_str("\"true\"").unwrap();
-        assert_eq!(type_check(exp).unwrap(), Type::Str);
+        assert_eq!(type_check(&exp).unwrap(), Type::Str);
 
         let exp = lexpr::from_str("\"foo\"").unwrap();
-        assert_eq!(type_check(exp).unwrap(), Type::Str);
+        assert_eq!(type_check(&exp).unwrap(), Type::Str);
 
         let exp = lexpr::from_str("\"\"").unwrap();
-        assert_eq!(type_check(exp).unwrap(), Type::Str);
+        assert_eq!(type_check(&exp).unwrap(), Type::Str);
+    }
+
+    #[test]
+    fn typecheck_binops() {
+        let exp = lexpr::from_str("(+ 3 5)").unwrap();
+        assert_eq!(type_check(&exp).unwrap(), Type::Int);
+
+        let exp = lexpr::from_str("(* 3 5)").unwrap();
+        assert_eq!(type_check(&exp).unwrap(), Type::Int);
+
+        let exp = lexpr::from_str("(- 3 5)").unwrap();
+        assert_eq!(type_check(&exp).unwrap(), Type::Int);
+
+        let exp = lexpr::from_str("(/ 3 5)").unwrap();
+        assert_eq!(type_check(&exp).unwrap(), Type::Int);
     }
 }
