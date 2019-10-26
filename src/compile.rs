1086385777644c594eca9db2e16a2edb03d4bf1a
Chriscbr <crybicki98@gmail.com>
2019-10-26 01:08:39 -0400

    Fix compile test utility

--- src/compile.rs
+++ src/compile.rs
@@ -2,17 +2,97 @@ use crate::closure_convert::closure_convert;
 use crate::generate_code::generate_code;
 use crate::lambda_lift::lambda_lift;
 use crate::parse::parse;
-use crate::type_check::type_check;
-use proc_macro2::TokenStream;
+use crate::type_check::{type_check, type_check_prog};
+
+static boilerplate: &str = r#"
+
+#[derive(Clone)]
+pub enum Val {
+    Int(i64),
+}
+
+impl std::fmt::Display for Val {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Val::Int(x) => write!(f, "{}", *x),
+        }
+    }
+}
+
+impl Val {
+    pub fn stringify(&self, heap: &Heap) -> String {
+        match self {
+            Val::Int(x) => format!("{}", *x),
+        }
+    }
+
+    pub fn unwrap_int(&self) -> i64 {
+        match self {
+            Val::Int(x) => *x,
+            _ => panic!("called `Val::unwrap_int` on something besides Val::Int")
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct Heap {
+    mem: Vec<Val>
+}
+
+impl From<Vec<Val>> for Heap {
+    fn from(mem: Vec<Val>) -> Self {
+        Heap { mem: mem }
+    }
+}
+
+impl Heap {
+    pub fn new() -> Self {
+        Heap { mem: vec![] }
+    }
+
+    pub fn alloc(&mut self, v: Val) -> usize {
+        self.mem.push(v);
+        self.mem.len() - 1
+    }
+
+    pub fn get(&self, i: usize) -> &Val {
+        &self.mem[i]
+    }
+
+    pub fn set(&mut self, i: usize, v: Val) {
+        self.mem[i] = v;
+    }
+
+    pub fn copy(&mut self, into: usize, from: usize) {
+        self.mem[into] = self.mem[from].clone();
+    }
+
+    pub fn next_alloc_addr(&self) -> usize {
+        self.mem.len()
+    }
+}
+
+fn run(h: &mut Heap) -> usize {
+    <EXP>
+}
+
+fn main() {
+    let mut h = Heap::new();
+    let result = run(&mut h);
+    println!("{}", h.get(result).stringify(&h));
+}
+"#;
 
 // TODO: rework to produce some kind of fallback if any step fails,
 // e.g. as done in
 // https://docs.rs/lexpr-macros/0.2.0/src/lexpr_macros/lib.rs.html
-pub fn compile(input: lexpr::Value) -> TokenStream {
+pub fn compile(input: lexpr::Value) -> String {
     let exp = parse(&input).unwrap();
     type_check(&exp).unwrap();
     let cc_exp = closure_convert(&exp).unwrap();
     type_check(&cc_exp).unwrap();
     let prog = lambda_lift(&cc_exp).unwrap();
-    generate_code(&prog).unwrap()
+    type_check_prog(&prog).unwrap();
+    let code = generate_code(&prog).unwrap();
+    boilerplate.replace("<EXP>", format!("{}", code).as_str())
 }
--- tests/compile.rs
+++ tests/compile.rs
@@ -1,97 +1,21 @@
-use proc_macro2::TokenStream;
 use scheme_to_rust::compile::compile;
 use std::{env, fs, process::Command};
 
-static boilerplate: &str = r#"
-
-#[derive(Clone)]
-pub enum Val {
-    Int(i64),
-}
-
-impl std::fmt::Display for Val {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Val::Int(x) => write!(f, "{}", *x),
-        }
-    }
-}
-
-impl Val {
-    pub fn stringify(&self, heap: &Heap) -> String {
-        match self {
-            Val::Int(x) => format!("{}", *x),
-        }
-    }
-
-    pub fn unwrap_int(&self) -> i64 {
-        match self {
-            Val::Int(x) => *x,
-            _ => panic!("called `Val::unwrap_int` on something besides Val::Int")
-        }
-    }
-}
-
-#[derive(Default)]
-pub struct Heap {
-    mem: Vec<Val>
-}
-
-impl From<Vec<Val>> for Heap {
-    fn from(mem: Vec<Val>) -> Self {
-        Heap { mem: mem }
-    }
-}
-
-impl Heap {
-    pub fn new() -> Self {
-        Heap { mem: vec![] }
-    }
-
-    pub fn alloc(&mut self, v: Val) -> usize {
-        self.mem.push(v);
-        self.mem.len() - 1
-    }
-
-    pub fn get(&self, i: usize) -> &Val {
-        &self.mem[i]
-    }
-
-    pub fn set(&mut self, i: usize, v: Val) {
-        self.mem[i] = v;
-    }
-
-    pub fn copy(&mut self, into: usize, from: usize) {
-        self.mem[into] = self.mem[from].clone();
-    }
-
-    pub fn next_alloc_addr(&self) -> usize {
-        self.mem.len()
-    }
-}
-
-fn run(h: &mut Heap) -> usize {
-    <EXP>
-}
-
-fn main() {
-    let mut h = Heap::new();
-    let result = run(&mut h);
-    println!("{}", h.get(result).stringify(&h));
-}
-"#;
-
-// TODO: fix so this test cleans itself up if it fails
-// (probably requires finding some crate to do this?)
-fn run_code(input: TokenStream) -> String {
-    let data = boilerplate.replace("<EXP>", format!("{}", input).as_str());
+fn run_code(input: String) -> String {
     let curr_dir = env::current_dir().expect("Could not get current directory.");
     let tmp_dir = curr_dir.join("tests").join("tmp");
     let tmp_file = tmp_dir.join("foo.rs");
-    // let data = String::from(r#"fn main() { print!("3"); }"#);
-    fs::create_dir(tmp_dir.clone()).expect("Unable to create tmp directory.");
-    fs::write(tmp_file.clone(), data).expect("Unable to write file.");
-    // let cleanup = Command::new("rustfmt")
+    fs::create_dir(tmp_dir.clone())
+        .or_else(|_err| {
+            fs::remove_dir_all(tmp_dir.clone()).expect("Unable to remove temp directory");
+            fs::create_dir(tmp_dir.clone())
+        })
+        .expect("Unable to set up temp directory.");
+    fs::write(tmp_file.clone(), input).expect("Unable to write file.");
+    let _cleanup = Command::new("rustfmt")
+        .arg(tmp_file.clone())
+        .output()
+        .expect("Unable to format rust code.");
     let prog = Command::new("rustc")
         .arg(tmp_file.clone())
         .arg("--out-dir")
@@ -119,6 +43,6 @@ fn test_compile_happy() {
     let code = compile(exp);
     println!("{}", code);
     let mut result = run_code(code);
-    result.pop(); // remove newline
+    result.pop(); // remove newline from program output
     assert_eq!(result, String::from("3"));
 }
