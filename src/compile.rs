188a0be3ae2db21aad3deb07b0f9c6bf7af6c515
Chriscbr <crybicki98@gmail.com>
2020-03-24 01:38:32 -0400

    Remove commented code and unneeded imports

--- src/compile.rs
+++ src/compile.rs
@@ -1,49 +1,9 @@
-use crate::closure_convert::{closure_convert, ClosureConvertError};
+use crate::closure_convert::closure_convert;
 use crate::common::{Expr, Prog, TypedExpr};
-use crate::generate_code::CodeGenerateError;
-use crate::lambda_lift::{lambda_lift, LambdaLiftError};
+use crate::lambda_lift::lambda_lift;
 use crate::pack_elim::pack_elim_prog;
-use crate::record_elim::{record_elim_prog, RecordElimError};
-use crate::type_check::{type_check, type_check_prog, TypeCheckError};
-
-// #[derive(Clone, Debug)]
-// pub struct CompilationError(Box<std::error::Error>);
-
-// #[derive(Clone, Debug)]
-// pub enum CompilationError {
-//     TypeCheck(TypeCheckError),
-//     ClosureConvert(ClosureConvertError),
-//     LambdaLift(LambdaLiftError),
-//     RecordElim(RecordElimError),
-//     CodeGenerate(CodeGenerateError),
-// }
-
-// // Allows other errors to wrap this one
-// impl std::error::Error for CompilationError {
-//     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-//         // Generic error, underlying cause isn't tracked.
-//         // None
-//         match self {
-//             Self::TypeCheck(err) => Some(err),
-//             Self::ClosureConvert(err) => Some(err),
-//             Self::LambdaLift(err) => Some(err),
-//             Self::RecordElim(err) => Some(err),
-//             Self::CodeGenerate(err) => Some(err),
-//         }
-//     }
-// }
-
-// impl std::fmt::Display for CompilationError {
-//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-//         match self {
-//             Self::TypeCheck(err) => write!(f, "CompilationError: {}", err),
-//             Self::ClosureConvert(err) => write!(f, "CompilationError: {}", err),
-//             Self::LambdaLift(err) => write!(f, "CompilationError: {}", err),
-//             Self::RecordElim(err) => write!(f, "CompilationError: {}", err),
-//             Self::CodeGenerate(err) => write!(f, "CompilationError: {}", err),
-//         }
-//     }
-// }
+use crate::record_elim::record_elim_prog;
+use crate::type_check::{type_check, type_check_prog};
 
 /// Perform a complete compilation from an Expr to a Prog - in other words, all
 /// all compiler passes before code generation.
